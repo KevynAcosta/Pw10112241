6497b99059b914fa5d9155cefd0007c3603c1901
KevynAcosta <159816410+KevynAcosta@users.noreply.github.com>
2024-04-25 10:51:11 -0700

    conexion a MySQL

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -6,11 +6,52 @@ let app  = express();
 app.use(express.json());
 app.use(cors());
 
+//Conexion a MySQL
+let conexion = mysql.createConnection(
+    {
+        host:'localhost',
+        user: 'root',
+        password: '',
+        database: 'pwdata',
+        port: '3306'
+});
+
+//Nos conectamos a MySQL
+conexion.connect(function(error)
+ {
+    if(error)
+    {
+        throw error;
+    }
+    else
+    {
+        console.log('Conectado a la base de datos')
+    }
+});
+
+
+
 //Rutas de acceso
 app.get("/",function(req,res) {
 
     res.send("Rutasssssss de iniciosssss");
 })
+
+//Seleccionamos todos los clientes
+app.get('/api/clientes',(req,res)=>
+{
+    conexion.query('Select * From clientes',(error,filas)=>
+    {
+        if(error)
+        {
+            throw error;
+        }
+        else
+        {
+            res.send(filas);
+        } 
+    });
+});
 
 //Encender el servidor
 let puerto = 3000;
